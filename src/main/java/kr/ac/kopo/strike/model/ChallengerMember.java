d03efce4b4450d818c97bcccd33515da0cbb789e
Student <Student@DESKTOP-VD2CFFR>
2022-04-29 17:02:48 +0900

    모델 추가

---
+++ src/main/java/kr/ac/kopo/strike/model/ChallengerMember.java
@@ -0,0 +1,157 @@
+package kr.ac.kopo.strike.model;
+
+public class ChallengerMember {
+	private int member_code;
+	private String id;
+	private String pw;
+	private String name;
+	private String birth;
+	private int sex;
+	private String tel;
+	private String clan;
+	private String tier;
+	private int record;
+	private int win;
+	private int lose;
+	private int draw;
+	private int rate;
+	private int challenger_code;
+	private int game_code;
+	private int state;
+
+	public int getMember_code() {
+		return member_code;
+	}
+
+	public void setMember_code(int member_code) {
+		this.member_code = member_code;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public int getSex() {
+		return sex;
+	}
+
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getClan() {
+		return clan;
+	}
+
+	public void setClan(String clan) {
+		this.clan = clan;
+	}
+
+	public String getTier() {
+		return tier;
+	}
+
+	public void setTier(String tier) {
+		this.tier = tier;
+	}
+
+	public int getRecord() {
+		return record;
+	}
+
+	public void setRecord(int record) {
+		this.record = record;
+	}
+
+	public int getWin() {
+		return win;
+	}
+
+	public void setWin(int win) {
+		this.win = win;
+	}
+
+	public int getLose() {
+		return lose;
+	}
+
+	public void setLose(int lose) {
+		this.lose = lose;
+	}
+
+	public int getDraw() {
+		return draw;
+	}
+
+	public void setDraw(int draw) {
+		this.draw = draw;
+	}
+
+	public int getRate() {
+		return rate;
+	}
+
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+
+	public int getChallenger_code() {
+		return challenger_code;
+	}
+
+	public void setChallenger_code(int challenger_code) {
+		this.challenger_code = challenger_code;
+	}
+
+	public int getGame_code() {
+		return game_code;
+	}
+
+	public void setGame_code(int game_code) {
+		this.game_code = game_code;
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
+}
